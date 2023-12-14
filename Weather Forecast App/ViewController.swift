07a08a0bf35e31805bff87e33ede49d1c24d9c11
Eric Kazuo <erickazuo@Erics-MacBook-Air.local>
2023-12-14 15:05:14 -0300

    first commit

---
+++ Weather Forecast App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Weather Forecast App
+//
+//  Created by Eric Kazuo  on 14/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
